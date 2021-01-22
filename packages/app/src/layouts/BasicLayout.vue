53f29f3a00bd849497d9cc6667d7550ccc8922e5
Mario9 <970426404@qq.com>
2021-01-22 22:16:42 +0800

    app

---
+++ packages/app/src/layouts/BasicLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="basic-layout">
+    <van-tabbar v-model="activeName">
+      <van-tabbar-item name="home" icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item name="search" icon="search">搜索</van-tabbar-item>
+      <van-tabbar-item name="firend" icon="friends-o">好友</van-tabbar-item>
+      <van-tabbar-item name="setting" icon="setting-o">设置</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+<script>
+import {
+  Tabbar,
+  TabbarItem
+} from 'vant'
+export default {
+  name: 'BasicLayout',
+  components: {
+    [Tabbar.name]: Tabbar,
+    [TabbarItem]: TabbarItem
+  },
+  data () {
+    return {
+      activeName: 'home'
+    }
+  }
+}
+</script>
---
+++ packages/app/src/layouts/BlankLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="blank-layout">
+    <van-nav-bar title="标题" left-arrow @click-left="onBack">
+      <template #right>
+        <slot name="right"></slot>
+      </template>
+    </van-nav-bar>
+    <div class="container">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script>
+import {
+  NavBar
+} from 'vant'
+export default {
+  name: 'BlankLayout',
+  components: {
+    [NavBar.name]: NavBar
+  },
+  methods: {
+    onBack () {
+
+    }
+  }
+}
+</script>
